98a27933518e7d3da736caa9235258e2184ac081
Tania <taniaor58@gmail.com>
2025-01-11 14:58:06 -0800

    added test file from homework and edited sys_call

--- proj1/user/calc.c
+++ proj1/user/calc.c
@@ -1,6 +1,6 @@
-#include "kernel/types.h"
-#include "user/user.h"
+// #include "kernel/types.h"
+// #include "user/user.h"
 
-int calculate(int x, int y, char* op, int* result) {
+// int calculate(int x, int y, char* op, int* result) {
     
-}
+// }
